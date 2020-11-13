9691d6d26b57572d1b1578c49f043c1f7b539c8b
nxxc <nomad.nxxc@gmail.com>
2020-11-13 14:54:26 +0900

    clean up folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './Row.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <h1>
+                Hey Clever Programmer ! Let's build netflix clone Front-end
+                today
+            </h1>
+            <Row />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Row.js
@@ -0,0 +1,6 @@
+import React from 'react';
+function Row() {
+    return <div></div>;
+}
+
+export default Row;
